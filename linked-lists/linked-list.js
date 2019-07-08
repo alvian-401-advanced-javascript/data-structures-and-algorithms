b818440ad672b1e0f1128d12e40123b92141daaf
AJ <alvianjoseph@gmail.com>
2019-07-08 00:38:47 -0700

    added insert, includes, and toString methods to list.js

--- linked-lists/linked-list.js
+++ linked-lists/linked-list.js
@@ -1,6 +1,39 @@
 class Node {
     constructor(value) {
-      this.value = value;
-      this.next = null;
+        this.value = value;
+        this.next = null;
     }
-  }
+}
+
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insert(value) {
+        let newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        return newNode;
+    };
+
+    includes(value) {
+        let currentNode = this.head;
+        let isInList = false;
+        while (currentNode !== null) {
+            if (currentNode.value === value) {
+                isInList = true;
+            };
+            currentNode = currentNode.next;
+        };
+        return isInList;
+    }
+
+    toString() {
+        while (currentNode !== null) {
+            console.log(currentNode.toString())
+        }
+
+    }
+}
