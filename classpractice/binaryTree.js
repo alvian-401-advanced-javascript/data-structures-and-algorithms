7ec81c676b41c7eaff05e65b790d354fa86b34bb
AJ <alvianjoseph@gmail.com>
2019-08-04 19:58:54 -0700

    trees implementation

--- classpractice/binaryTree.js
+++ classpractice/binaryTree.js
@@ -5,7 +5,7 @@ const { Stack, Queue } = require('../data-structures/stacks-and-queues/stacks-an
 
 
 class BinaryTree {
-  constructor(root=null) {
+  constructor(root = null) {
     this.root = root;
   }
 
@@ -14,9 +14,9 @@ class BinaryTree {
 
     let _walk = (node) => {
       results.push(node.value);
-      if(node.left) { _walk(node.left) }
-      
-      if(node.right) { _walk(node.right) }
+      if (node.left) { _walk(node.left); }
+
+      if (node.right) { _walk(node.right); }
     };
     _walk(this.root);
 
@@ -27,9 +27,9 @@ class BinaryTree {
     let results = [];
 
     let _walk = (node) => {
-      if(node.left) { _walk(node.left) }
+      if (node.left) { _walk(node.left); }
 
-      if(node.right) { _walk(node.right) }
+      if (node.right) { _walk(node.right); }
 
       results.push(node.value);
 
@@ -39,36 +39,23 @@ class BinaryTree {
     return results;
   }
 
- 
+
 
   inOrder() {
     let results = [];
 
     let _walk = (node) => {
-      if(node.left) { _walk(node.left) }
+      if (node.left) { _walk(node.left); }
 
       results.push(node.value);
 
-      if(node.right) { _walk(node.right) }
+      if (node.right) { _walk(node.right); }
     };
     _walk(this.root);
 
     return results;
   }
 
-  levelOrder() {
-    
-    let queue = new Queue();
-    queue.enqueue(this.root);
-
-    while(queue.length) {
-      
-    }
-  }
 }
 
-
-
-
-let ten = new Node(10);
 
---
+++ data-structures/trees/binary-tree.js
@@ -0,0 +1,117 @@
+'use strict';
+
+// const Node = require('../linked-lists/linked-list.js');
+const { Stack, Queue } = require('../stacks-and-queues/stacks-and-queues-with-nodes.js');
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor(value) {
+    this.root = null;
+    if (value) {
+      this.root = new Node(value);
+    }
+  }
+
+  add(value) {
+    let node = new Node(value);
+    if (!this.root) {
+      this.root = node;
+    }
+    if (this.root.value < value) {
+      this.root.right = new Node(value);
+    } else {
+      this.root.left = new Node(value);
+    }
+  }
+
+  preOrder() {
+    let results = new Queue();
+
+    let _walk = (node) => {
+      results.enqueue(node.value);
+      if (node.left) { _walk(node.left); }
+
+      if (node.right) { _walk(node.right); }
+    };
+    _walk(this.root);
+
+    return results;
+  }
+
+  postOrder() {
+    let results = new Queue();
+
+    let _walk = (node) => {
+      if (node.left) { _walk(node.left); }
+
+      if (node.right) { _walk(node.right); }
+
+      results.enqueue(node.value);
+
+    };
+    _walk(this.root);
+
+    return results;
+  }
+
+  inOrder() {
+    let results = new Queue();
+
+    let _walk = (node) => {
+      if (node.left) { _walk(node.left); }
+
+      results.enqueue(node.value);
+
+      if (node.right) { _walk(node.right); }
+    };
+    _walk(this.root);
+
+    return results;
+  }
+
+}
+
+class BinarySearchTree {
+
+  constructor(value) {
+    this.root = null;
+    if (value) {
+      this.root = new Node(value);
+    }
+  }
+
+  add(value) {
+    let node = new Node(value);
+    if (!this.root) {
+      this.root = node;
+    }
+    if (this.root.value < value) {
+      this.root.right = new Node(value);
+    } else {
+      this.root.left = new Node(value);
+    }
+  }
+
+  contains(value, current = this.root) {
+    if (current === null || value === undefined) {
+      return false;
+    }
+    if (current.value === value) {
+      return true;
+    } else if (current.value > value) {
+      return this.contains(value, current.left);
+    } else {
+      return this.contains(value, current.right);
+    }
+  }
+}
+
+
+module.exports = { BinaryTree, BinarySearchTree, Node, Queue };
